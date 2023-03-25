fac21be596dcd527ceb05bec97524ee4c43aebb6
beejeys <beejeysa@gmail.com>
2023-03-25 13:54:25 +0530

    all files

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('db', function () {
+
+	$users = DB::select("SELECT * FROM users");
+	dd($users);
 });
